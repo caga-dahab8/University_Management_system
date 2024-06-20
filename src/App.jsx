6c83d348151a09309980fe8f830e792dc42c390d
abdirashidRash <ilmojini9@gmail.com>
2024-06-20 15:58:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Pages/Login'
+
+function App() {
+
+  return (
+    <>
+      {/* Login page */}
+      <Login/>
+    </>
+  )
+}
+
+export default App
